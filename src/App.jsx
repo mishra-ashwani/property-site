defdd0418d4155d784a19204be69f65479f4b2fc
Ashwani Mishra <mishra.ashwani1989@gmail.com>
2024-02-11 21:07:15 +0530

    Add Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Signout from './pages/Signout';
+import Profile from './pages/Profile';
 
 export default function App() {
-  return (
-    <h1 className="text-red-500">Hello</h1>
-  )
+  return <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signout' element={<Signout />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>;
+  
 }
